e6a056b340d201ef79818a5b064bdf6e063a48db
Amirsepehr <amirsepehr.shamloo.1996@gmail.com>
2026-08-12 06:04:37 -0700

    Some new revisions and 5-fold stratified cross validation modeling and model is logistic regression

--- src/logisticregression_training.py
+++ src/logisticregression_training.py
@@ -1,49 +1,61 @@
 import joblib
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import recall_score, precision_score, f1_score
 
 # Load the processed data
-data = joblib.load(r"E:\maktab_sharif\mini_project1_week9\mini-project-01\data\processed_data.pkl")
+data = joblib.load(
+    r"E:\maktab_sharif\mini_project1_week9\mini-project-01\data\processed_data.pkl"
+)
 
 X = data["X_train"]
 y = data["y_train"]
-scaler = data["scaler"]
 
-skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
+# 5-Fold Stratified Cross Validation
+skf = StratifiedKFold(
+    n_splits=5,
+    shuffle=True,
+    random_state=42
+)
 
 recalls = []
 precisions = []
-f_1_scores = []
+f1_scores = []
+
+print(type(X))
+print(type(y))
 
 for train_idx, test_idx in skf.split(X, y):
-    X_train, y_train = X.iloc[train_idx], y.iloc[train_idx]
-    X_test, y_test = X.iloc[test_idx], y.iloc[test_idx]
 
-    LR = LogisticRegression(random_state=42, max_iter=1000)
-    LR.fit(X_train, y_train)
+    X_train = X[train_idx]
+    X_test = X[test_idx]
 
-    y_hat = LR.predict(X_test)
+    y_train = y.iloc[train_idx]
+    y_test = y.iloc[test_idx]
 
-    recall = recall_score(y_test, y_hat)
-    precision = precision_score(y_test, y_hat)
-    f_1_score = f1_score(y_test, y_hat)
+    model = LogisticRegression(
+        random_state=42,
+        max_iter=1000
+    )
 
-    recalls.append(recall)
-    precisions.append(precision)
-    f_1_scores.append(f_1_score)
+    model.fit(X_train, y_train)
 
+    y_pred = model.predict(X_test)
+
+    recalls.append(recall_score(y_test, y_pred))
+    precisions.append(precision_score(y_test, y_pred))
+    f1_scores.append(f1_score(y_test, y_pred))
 
 results = pd.DataFrame({
-    "Mean_of_Recalls": [np.mean(recalls)],
-    "Std_of_Recalls": [np.std(recalls)],
-    "Mean_of_Precisions": [np.mean(precisions)],
-    "Std_of_Precisions": [np.std(precisions)],
-    "Mean_of_F1-Score": [np.mean(f_1_scores)],
-    "Std_of_F1-Score": [np.std(f_1_scores)]
+    "Mean Recall": [np.mean(recalls)],
+    "Std Recall": [np.std(recalls)],
+    "Mean Precision": [np.mean(precisions)],
+    "Std Precision": [np.std(precisions)],
+    "Mean F1-Score": [np.mean(f1_scores)],
+    "Std F1-Score": [np.std(f1_scores)]
 })
 
-
-results
+print(results)
