828224ac15d9c0bab92dfcf8f1c2b47b6343e299
Amirsepehr <amirsepehr.shamloo.1996@gmail.com>
2026-08-12 14:04:21 -0700

    training decision tree optimum model is now completed

--- src/dt.py
+++ src/dt.py
@@ -18,56 +18,56 @@ y = data["y_train"]
 print(type(X))
 print(type(y))
 
-# 5-Fold Stratified Cross Validation
-# skf = StratifiedKFold(
-#     n_splits=5,
-#     shuffle=True,
-#     random_state=42
-# )
+5-Fold Stratified Cross Validation
+skf = StratifiedKFold(
+    n_splits=5,
+    shuffle=True,
+    random_state=42
+)
 
-# Depths = [2, 5, 10, 15, None]
+Depths = [2, 5, 10, 15, None]
 
-# results = []
+results = []
 
-# for depth in Depths:
+for depth in Depths:
 
-#     recalls = []
-#     precisions = []
-#     f1_scores = []
+    recalls = []
+    precisions = []
+    f1_scores = []
 
-#     for train_idx, test_idx in skf.split(X, y):
+    for train_idx, test_idx in skf.split(X, y):
 
-#         X_train = X[train_idx]
-#         X_test = X[test_idx]
+        X_train = X[train_idx]
+        X_test = X[test_idx]
 
-#         y_train = y.iloc[train_idx]
-#         y_test = y.iloc[test_idx]
+        y_train = y.iloc[train_idx]
+        y_test = y.iloc[test_idx]
 
-#         model = DecisionTreeClassifier(
-#             max_depth=depth, random_state=42
-#         )
+        model = DecisionTreeClassifier(
+            max_depth=depth, random_state=42
+        )
 
-#         model.fit(X_train, y_train)
+        model.fit(X_train, y_train)
 
-#         y_pred = model.predict(X_test)
+        y_pred = model.predict(X_test)
 
-#         recalls.append(recall_score(y_test, y_pred))
-#         precisions.append(precision_score(y_test, y_pred))
-#         f1_scores.append(f1_score(y_test, y_pred))
+        recalls.append(recall_score(y_test, y_pred))
+        precisions.append(precision_score(y_test, y_pred))
+        f1_scores.append(f1_score(y_test, y_pred))
 
-#     results.append({
-#         "Max Depth": depth,
-#         "Mean Recall": np.mean(recalls),
-#         "Std Recall": np.std(recalls),
-#         "Mean Precision": np.mean(precisions),
-#         "Std Precision": np.std(precisions),
-#         "Mean F1-Score": np.mean(f1_scores),
-#         "Std F1-Score": np.std(f1_scores)
-#     })
+    results.append({
+        "Max Depth": depth,
+        "Mean Recall": np.mean(recalls),
+        "Std Recall": np.std(recalls),
+        "Mean Precision": np.mean(precisions),
+        "Std Precision": np.std(precisions),
+        "Mean F1-Score": np.mean(f1_scores),
+        "Std F1-Score": np.std(f1_scores)
+    })
 
-# results = pd.DataFrame(results)
+results = pd.DataFrame(results)
 
-# print(results)
+print(results)
 
 
 # Training using all train data
@@ -87,23 +87,23 @@ disp.plot(cmap="Blues", values_format="d")
 plt.title("Confusion Matrix - DT (Optimum Max_Depth = 5)")
 plt.show()
 
-# # Manual calculation
-# TN, FP, FN, TP = 226593, 9, 63, 315
+# Manual calculation
+TN, FP, FN, TP = 226583, 19, 69, 309
 
-# Recall_M = TP / (TP + FN)
-# Precision_M = TP / (TP + FP)
-# F1_M = (2 * Recall_M * Precision_M) / (Recall_M + Precision_M)
+Recall_M = TP / (TP + FN)
+Precision_M = TP / (TP + FP)
+F1_M = (2 * Recall_M * Precision_M) / (Recall_M + Precision_M)
 
-# # Scikit-learn calculation
-# Recall_A = recall_score(y, y_hat)
-# Precision_A = precision_score(y, y_hat)
-# F1_A = f1_score(y, y_hat)
+# Scikit-learn calculation
+Recall_A = recall_score(y, y_hat)
+Precision_A = precision_score(y, y_hat)
+F1_A = f1_score(y, y_hat)
 
-# # Compare results
-# final_training_metrics = pd.DataFrame({
-#     "Metric": ["Recall", "Precision", "F1-Score"],
-#     "Manual": [Recall_M, Precision_M, F1_M],
-#     "Scikit-learn": [Recall_A, Precision_A, F1_A]
-# })
+# Compare results
+final_training_metrics = pd.DataFrame({
+    "Metric": ["Recall", "Precision", "F1-Score"],
+    "Manual": [Recall_M, Precision_M, F1_M],
+    "Scikit-learn": [Recall_A, Precision_A, F1_A]
+})
 
-# print(final_training_metrics)
+print(final_training_metrics)
