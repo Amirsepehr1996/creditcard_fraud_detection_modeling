189ffbc90cb6c5792fea2c47d58b0d9f1a45520c
Amirsepehr <amirsepehr.shamloo.1996@gmail.com>
2026-08-12 07:38:45 -0700

    Training on whole training dataset and creating final metrics for training

--- src/logisticregression_training.py
+++ src/logisticregression_training.py
@@ -1,10 +1,11 @@
 import joblib
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 
 # Load the processed data
 data = joblib.load(
@@ -59,3 +60,42 @@ results = pd.DataFrame({
 })
 
 print(results)
+
+
+# Training using all train data
+Model = LogisticRegression(random_state=42, max_iter=1000)
+Model.fit(X, y)
+y_hat = Model.predict(X)
+cm = confusion_matrix(y, y_hat)
+print(cm)
+
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm,
+    display_labels=["Normal (0)", "Fraud (1)"]
+)
+
+disp.plot(cmap="Blues", values_format="d")
+
+plt.title("Confusion Matrix - Logistic Regression")
+plt.show()
+
+# Manual calculation
+TN, FP, FN, TP = 226569, 33, 148, 230
+
+Recall_M = TP / (TP + FN)
+Precision_M = TP / (TP + FP)
+F1_M = (2 * Recall_M * Precision_M) / (Recall_M + Precision_M)
+
+# Scikit-learn calculation
+Recall_A = recall_score(y, y_hat)
+Precision_A = precision_score(y, y_hat)
+F1_A = f1_score(y, y_hat)
+
+# Compare results
+final_training_metrics = pd.DataFrame({
+    "Metric": ["Recall", "Precision", "F1-Score"],
+    "Manual": [Recall_M, Precision_M, F1_M],
+    "Scikit-learn": [Recall_A, Precision_A, F1_A]
+})
+
+print(final_training_metrics)
